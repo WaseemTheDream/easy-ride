3fcc077dbea24009264721133953976c0a258a6f
Marco <mr.marco.lobo@gmail.com>
2013-02-01 16:50:36 +0000

    as

--- easy-ride/index.php
+++ easy-ride/index.php
@@ -3,10 +3,12 @@
   <?php include "head.php"?>
   <body>
     <?php  include "navbar.php"?>
+    <?php  include "home.php"?>
     <div class="container-fluid">
   			<div class="row-fluid">
     			<div class="span2">
      				 <!--Sidebar content-->
+     				 
     			</div>
     			<div class="span10">
      				 <!--Body content-->
