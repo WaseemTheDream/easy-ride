ad4cc280f750085704bbb1903f4e02d2b54c5e76
Waseem Ahmad <waseem@rice.edu>
2013-02-01 17:11:52 +0000

    Changed login form

--- easy-ride/navbar.php
+++ easy-ride/navbar.php
@@ -7,10 +7,10 @@ function login() {
 echo <<<_END
   <a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign In <strong class="caret"></strong></a>
   <div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
-    <form action="[YOUR ACTION]" method="post" accept-charset="UTF-8">
-      <input id="user_username" style="margin-bottom: 15px;" type="text" name="user[username]" size="30" />
-      <input id="user_password" style="margin-bottom: 15px;" type="password" name="user[password]" size="30" />
-      <input id="user_remember_me" style="float: left; margin-right: 10px;" type="checkbox" name="user[remember_me]" value="1" />
+    <form method="post" accept-charset="UTF-8">
+      <input id="email" style="margin-bottom: 15px;" type="text" name="email" size="30" placeholder="Email Address" />
+      <input id="password" style="margin-bottom: 15px;" type="password" name="password" size="30" placeholder="Password" />
+      <input id="remember-me" style="float: left; margin-right: 10px;" type="checkbox" name="remember-me" value="1" />
       <label class="string optional" for="user_remember_me"> Remember me</label>
       <input class="btn btn-primary" style="clear: left; width: 100%; height: 32px; font-size: 13px;" type="submit" name="commit" value="Sign In" />
     </form>
