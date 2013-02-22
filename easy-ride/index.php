53f9f8dc1c184a94d808b3ad2580dced30da0bdd
Waseem Ahmad <waseem@rice.edu>
2013-02-22 15:37:31 +0000

    Fixed the positioning of the map canvas

--- easy-ride/index.php
+++ easy-ride/index.php
@@ -8,38 +8,35 @@
             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAeuwEG1p2ewZFCY6Xt5pHKuBlOElPpUVw&sensor=true">
   </script>
   <script src="js/index.js"></script>
-	<body onload="initialize()">
+  <body onload="initialize()">
    
      <div class="container-fluid">
-  			<div class="row-fluid">
-    			<div class="container span4">
-     				<!--Sidebar content-->
+        <div class="row-fluid">
+          <div class="container span4">
+            <!--Sidebar content-->
             <form class="form-horizontal well" id="search">
             <fieldset>
-       				<!-- From -->
-       		<div class="form-input">
-              <label>From</label>
-              <input class="input-xlarge" type="text" placeholder="Type an address or zip code over here">
-        	</div>			
-              		<!-- To -->
-            <div class="form-input">
-              <label>To</label>
-              <input class="input-xlarge" type="text" placeholder="Type an address or zip code over here">
-			</div>
-      			</div>
+              <!-- From -->
+              <div class="form-input">
+                <label>From</label>
+                <input class="input-xlarge" type="text" placeholder="Type an address or zip code over here">
+              </div>      
+              <!-- To -->
+              <div class="form-input">
+                <label>To</label>
+                <input class="input-xlarge" type="text" placeholder="Type an address or zip code over here">
+              </div>
             </fieldset>
             </form><!-- End Sidebar Content -->
-
           </div>
+          <div id="map_canvas" class="well container span8" style="float: right; height: 500px;"></div>
         </div>
-          
-  			<div id="map_canvas" class="container span8" style="float: right; height: 500px;">
-  			</div>
-			</div> 
-		</div>
-		
+      </div>
+    </div> 
+  </div>
     
-	<hr>
+    
+  <hr>
     <?php include "footer.php"?>
     </div>
   </body>
