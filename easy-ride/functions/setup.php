8937f829d06a4a3aa6f147d5320caf91f4112890
Waseem Ahmad <waseem@rice.edu>
2013-03-04 23:40:08 +0000

    Cleaned up setup.php

--- easy-ride/functions/setup.php
+++ easy-ride/functions/setup.php
@@ -3,14 +3,22 @@
 <?php
 require_once "functions.php";
 require_once "user.php";
+require_once "database.php";
 
 $connection = mysql_connect($dbhost, $dbuser, $dbpass);
 mysql_select_db($dbname, $connection);
 
-if (!mysql_query("CREATE TABLE IF NOT EXISTS " . $user_table_definition))
-	die ("Failed to create user table: " . mysql_error());
-else
-	echo "Created table ".USER_TABLE." if it didn't exist.";
+$tables = array(
+	USER_TABLE => $user_table_definition,
+	TRIP_TABLE => $trip_table_definition,
+	PLACE_TABLE => $place_table_definition);
+
+foreach ($tables as $name => $definition) {
+	if (!mysql_query("CREATE TABLE IF NOT EXISTS $definition"))
+		die ("Failed to create table: $name.");
+	else
+		echo "Created table $name if it didn't exist.<br>";
+}
 
 mysql_close($connection);
 
